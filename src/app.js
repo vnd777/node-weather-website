763fa3049d614820ff3c7cef5d70694cd2090418
vnd777 <51835466+vnd777@users.noreply.github.com>
2020-04-03 22:51:19 +0530

    Help content changed

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about',(req,res) =>{
 
 app.get('/help',(req,res) =>{
     res.render('help',{
-        helpText: 'This is some useful text!',
+        helpText: 'Just provide your location and get weather report!',
         title: 'Help',
         name: 'M Vinod'
     })
